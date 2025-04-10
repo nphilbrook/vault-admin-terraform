fa56ab2dfff79b731262eaa5fc0210a9e818c321
nphilbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-10 14:58:40 -0400

    Create AWS secrets engine

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 module "cloud_operations" {
   # source  = "app.terraform.io/philbrook/bu-namespace/vault"
   # version = "1.0.0"
-  source              = "github.com/nphilbrook/terraform-vault-bu-namespace"
-  name                = "Cloud-Operations"
-  auth_mount_accessor = data.vault_generic_secret.saml_mount.data.accessor
+  source                        = "github.com/nphilbrook/terraform-vault-bu-namespace"
+  name                          = "Cloud-Operations"
+  auth_mount_accessor           = data.vault_generic_secret.saml_mount.data.accessor
+  configure_aws                 = true
+  initial_aws_access_key_id     = var.initial_aws_access_key_id
+  initial_aws_secret_access_key = var.initial_aws_secret_access_key
 }

51801bc4b47112997984227a164dcfc9e244e399
nphilbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-15 15:47:38 -0400

    K/V policies (#8)

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "cloud_operations" {
   source  = "app.terraform.io/philbrook/bu-namespace/vault"
-  version = "1.0.1"
-  # source                        = "github.com/nphilbrook/terraform-vault-bu-namespace"
+  version = "1.0.2"
+  # source = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_policies"
   name                          = "Cloud-Operations"
   auth_mount_accessor           = data.vault_generic_secret.saml_mount.data.accessor
   configure_aws                 = true

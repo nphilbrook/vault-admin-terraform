5369cbd0dde0107d4828bc89dd58744f94c16f5c
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-15 15:46:29 -0400

    Remove local source line

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "cloud_operations" {
   source  = "app.terraform.io/philbrook/bu-namespace/vault"
   version = "1.0.2"
   # source = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_policies"
-  # source                        = "/home/nphilbrook/repos/bankunited/terraform-vault-bu-namespace"
   name                          = "Cloud-Operations"
   auth_mount_accessor           = data.vault_generic_secret.saml_mount.data.accessor
   configure_aws                 = true

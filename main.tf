48765ab0bb498b36ff0b22834ecdf6a73688d9e1
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-22 07:35:51 -0400

    Test AWS renaming

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "cloud_operations" {
-  source  = "app.terraform.io/philbrook/bu-namespace/vault"
-  version = "1.0.3"
-  # source                        = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
+  # source  = "app.terraform.io/philbrook/bu-namespace/vault"
+  # version = "1.0.3"
+  source                        = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
   name                          = "Cloud-Operations"
   auth_mount_accessor           = data.vault_generic_secret.saml_mount.data.accessor
   configure_aws                 = true

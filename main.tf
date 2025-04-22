8eac9a1d5e066fe1fc17f75cde372f93c07fee80
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-22 07:44:55 -0400

    bump to module

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "cloud_operations" {
-  # source  = "app.terraform.io/philbrook/bu-namespace/vault"
-  # version = "1.0.3"
-  source                        = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
+  source  = "app.terraform.io/philbrook/bu-namespace/vault"
+  version = "1.1.0"
+  # source                        = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
   name                          = "Cloud-Operations"
   auth_mount_accessor           = data.vault_generic_secret.saml_mount.data.accessor
   configure_aws                 = true
@@ -10,9 +10,9 @@ module "cloud_operations" {
 }
 
 module "appdev" {
-  # source              = "app.terraform.io/philbrook/bu-namespace/vault"
-  # version             = "1.0.3"
-  source              = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
+  source  = "app.terraform.io/philbrook/bu-namespace/vault"
+  version = "1.1.0"
+  # source              = "github.com/nphilbrook/terraform-vault-bu-namespace?ref=nphilbrook_gha"
   name                = "AppDev"
   configure_gha       = true
   auth_mount_accessor = data.vault_generic_secret.saml_mount.data.accessor

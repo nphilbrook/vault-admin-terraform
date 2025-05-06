e36ef0b4e59f4ef819180c069b73f2baa47d0fee
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-06 16:02:27 -0400

    rename SAML auth mount resource

--- saml_auth.tf
+++ saml_auth.tf
@@ -1,24 +1,22 @@
 # Fetch the current namespace, needed for the ACS Callback URL below
 data "vault_namespace" "current" {}
 
-resource "vault_saml_auth_backend" "auth0" {
+resource "vault_saml_auth_backend" "saml" {
   idp_metadata_url = "https://dev-duhq6zvtxhjtgss0.us.auth0.com/samlp/metadata/djIEvjayGHtOJsVaaZwOWhiHvQkmaSob"
   entity_id        = "${var.vault_address}/v1/auth/saml"
-  acs_urls = [
-    "${var.vault_address}/v1/${data.vault_namespace.current.id}auth/saml/callback",
-  "${var.vault_proxy_address}/v1/${data.vault_namespace.current.id}auth/saml/callback"]
-  default_role = "default"
+  acs_urls         = ["${var.vault_address}/v1/${data.vault_namespace.current.id}auth/saml/callback"]
+  default_role     = "default"
 }
 
 # The mount accessor is not exported as an attribute on the above resource,
 # but we can fetch it.
 # It's frustrating that we have to do this
 data "vault_generic_secret" "saml_mount" {
-  path = "sys/auth/${vault_saml_auth_backend.auth0.path}"
+  path = "sys/auth/${vault_saml_auth_backend.saml.path}"
 }
 
 resource "vault_saml_auth_backend_role" "default" {
-  path             = vault_saml_auth_backend.auth0.path
+  path             = vault_saml_auth_backend.saml.path
   name             = "default"
   token_policies   = ["default"]
   groups_attribute = "http://schemas.auth0.com/vault-roles"
@@ -28,7 +26,7 @@ resource "vault_saml_auth_backend_role" "default" {
 
 # break glass super admin
 resource "vault_saml_auth_backend_role" "break_glass" {
-  path             = vault_saml_auth_backend.auth0.path
+  path             = vault_saml_auth_backend.saml.path
   name             = "break-glass"
   token_policies   = ["break-glass"]
   groups_attribute = "http://schemas.auth0.com/vault-roles"

4d7ed4d31d10c35d5b3119a82462c8173682c0f3
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-09 13:36:47 -0400

    Doormat AWS support

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ locals {
 module "bu_namespaces" {
   for_each                      = local.namespace_configs
   source                        = "app.terraform.io/philbrook/bu-namespace/vault"
-  version                       = "1.2.1"
+  version                       = "1.3.0"
   name                          = each.key
   configure_gha                 = try(each.value.configure_gha, false)
   gha_org                       = try(each.value.gha_org, null)

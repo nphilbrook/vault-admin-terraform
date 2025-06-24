1f37af9b252b002d00b2eba8c5cad2392ebde4f2
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-24 10:05:32 -0400

    test out removing the workflow

--- integrations.tf
+++ integrations.tf
@@ -8,6 +8,7 @@ resource "vault_jwt_auth_backend" "int_jwt_gha" {
 
 module "gha_oidc_access" {
   source               = "app.terraform.io/philbrook/gha-access/vault"
+  version              = "1.0.1"
   vault_namespace_path = module.bu_namespaces["Integrations"].path
   jwt_backend_path     = vault_jwt_auth_backend.int_jwt_gha.path
   github_organization  = "nphilbrook"

1a5bd738ab0bceafb2f8a15b17f8025a6e56a150
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-30 11:57:36 -0400

    test policy parameteriziation

--- integrations.tf
+++ integrations.tf
@@ -7,18 +7,22 @@ resource "vault_jwt_auth_backend" "int_jwt_gha" {
 }
 
 module "gha_jwt_worklfow" {
+  #source               = "../terraform-vault-gha-access"
   source               = "app.terraform.io/philbrook/gha-access/vault"
-  version              = "1.0.3"
+  version              = "1.1.0"
   vault_namespace_path = module.bu_namespaces["Integrations"].path
   jwt_backend_path     = vault_jwt_auth_backend.int_jwt_gha.path
   role_name            = "gha-workflow"
   github_organization  = "nphilbrook"
   github_repositories  = ["*"]
+  write_paths          = ["nonprod/kv/*"]
+  read_paths           = ["prod/kv/*"]
 }
 
 module "gha_jwt_workflow_2" {
+  #source               = "../terraform-vault-gha-access"
   source               = "app.terraform.io/philbrook/gha-access/vault"
-  version              = "1.0.3"
+  version              = "1.1.0"
   vault_namespace_path = module.bu_namespaces["Integrations"].path
   jwt_backend_path     = vault_jwt_auth_backend.int_jwt_gha.path
   role_name            = "gha-test-addditional"

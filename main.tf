6a80c80217aa03abb37c1a4957865cb60f1cd38f
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-07-01 12:37:08 -0400

    Excuse me, THIS will be broken

--- main.tf
+++ main.tf
@@ -6,8 +6,9 @@ locals {
       kv_group_prod_name = "38260e06-2511-4def-91c2-e37327677264"
     },
     "AppDev" = {
-      kv_group_nonprod_name = "bd093061-2de9-445b-bd6c-fac871aead0b"
-      kv_group_prod_name    = "a63d1663-8cfd-4007-bcc6-74b77a21586b"
+      kv_group_nonprod_name      = "bd093061-2de9-445b-bd6c-fac871aead0b"
+      kv_group_prod_name         = "a63d1663-8cfd-4007-bcc6-74b77a21586b"
+      namespace_admin_group_name = "a63d1663-8cfd-4007-bcc6-74b77a21586b"
     },
     "Integrations" = {
     },
@@ -22,13 +23,14 @@ locals {
 
 module "bu_namespaces" {
   for_each = local.namespace_configs
-/*   source   = "app.terraform.io/philbrook/bu-namespace/vault"
+  /*   source   = "app.terraform.io/philbrook/bu-namespace/vault"
   version  = "3.1.0" */
   source = "git@github.com:nphilbrook/terraform-vault-bu-namespace.git?ref=main"
   # source                        = "/home/nphilbrook/repos/bankunited/terraform-vault-bu-namespace"
-  name                  = each.key
-  kv_group_prod_name    = try(each.value.kv_group_prod_name, null)
-  kv_group_nonprod_name = try(each.value.kv_group_nonprod_name, null)
+  name                       = each.key
+  kv_group_prod_name         = try(each.value.kv_group_prod_name, null)
+  kv_group_nonprod_name      = try(each.value.kv_group_nonprod_name, null)
+  namespace_admin_group_name = try(each.value.namespace_admin_group_name, null)
   # Common to all
   auth_mount_accessor = data.vault_auth_backend.saml_mount.accessor
 }
